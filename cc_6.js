9087f7cf75546521256bb3c8d841c12d7467e691
Tristan Zybala <tzybala@usf.edu>
2025-10-05 21:17:54 -0400

    Subclass created to extend Employee class

--- cc_6.js
+++ cc_6.js
@@ -11,3 +11,15 @@ class Employee {
         return `${this.name} works in ${this.department}`;
     }
 }
+
+// Step 3
+class Manager extends Employee {
+    constructor(name, department, teamSize) {
+        super(name, department);
+        this.teamSize = teamSize;
+    }
+
+    describe() {
+        return `${this.name} manages a team of ${this.teamSize} in ${this.department}`;
+    }
+}
